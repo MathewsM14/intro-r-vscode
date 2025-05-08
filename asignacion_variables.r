100ddf8e259a10690bc8222c475c06b5b53f1139
MathewsM14 <matmarquez@unal.edu.co>
2025-05-08 09:38:19 -0500

    Update asignacion_variables.r

--- asignacion_variables.r
+++ asignacion_variables.r
@@ -1,3 +1,25 @@
-#Lo primero que uno hace es borrar las variables que hay en el espacio de trabajo antes de comenzar a trabajar, esto se hace para evitar que cometamos el error de usar variables asignadas de trabajos anteriores y que por tanto nos funcione el cosigo pero no como deberia
+## Lo primero que uno hace es borrar las variables que hay en el espacio de trabajo antes de comenzar a trabajar, esto se hace para evitar que cometamos el error de usar variables asignadas de trabajos anteriores y que por tanto nos funcione el cosigo pero no como deberia
 
-rm(list=ls())  #Borrar variables del area de trabajo
+rm(list = ls())  #Borrar variables del area de trabajo
+
+#se pueden paralelizar las asignaciones
+
+#se pueden asignar procesos
+rm(list = ls())
+a <- 5
+b <- 12
+
+p <- (a+b) / 2
+p
+
+rm(list = ls())
+
+p <- function(a, b){
+    return((a+b)/2)
+}
+
+a <- 5
+b <- 12
+
+p(a, 4) -> val
+val
